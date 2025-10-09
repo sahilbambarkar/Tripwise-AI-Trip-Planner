a70612113aca688611b1a0f2edd68b1325b21f18
Sahil Bambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2025-10-09 15:23:29 +0530

    removed unneccesary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
 import Hero from './components/ui/custom/Hero'
 
 function App() {
